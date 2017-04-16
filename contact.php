2494fc52e9690e6f5772bac63e5a56d785a610a9
gilesjohn <gilesjohn@hotmail.co.uk>
2017-04-16 18:08:25 +0100

    Created contact.php and set out basic html

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<form action="" method="POST">
+	    	<label for="name">Name</label><br>
+	    	<input type="text" id="name" name="name" placeholder="Your name...">
+			<br>
+	    	<label for="email">Email</label><br>
+	    	<input type="text" id="email" name="email" placeholder="Your email...">
+			<br>
+			<label for="subject">Subject</label><br>
+	    	<input type="text" id="subject" name="subject" placeholder="Subject...">
+	    	<br>
+	    	<label for="subject">Message</label><br>
+	    	<textarea id="message" name="message" placeholder="Your message..." rows="4" cols="50"></textarea>
+	    	<br>
+	    	<input type="submit" value="Submit">
+	  </form>
+	</body>
+</html>
